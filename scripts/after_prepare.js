91a51dc1407bbf9c2686c2e0d8cbae1e7082b01e
Kieran David Evans <keyz182@gmail.com>
2018-08-07 16:52:41 +0100

    Only use package.json for cordova < 7

--- scripts/after_prepare.js
+++ scripts/after_prepare.js
@@ -1,26 +1,45 @@
 #!/usr/bin/env node
 'use strict';
 
-var fs = require('fs');
+module.exports = function(context) {
+    // If Cordova >= 7, check package.json
+    var fs = require('fs');
 
-if(process.argv.join("|").indexOf("APP_ID=") > -1) {
-	var APP_ID = process.argv.join("|").match(/APP_ID=(.*?)(\||$)/)[1]
-} else {
-	var config = fs.readFileSync("package.json").toString();
-	var json = JSON.parse(config);
-	var APP_ID = json["cordova"]["plugins"]["cordova-plugin-facebook4"]["APP_ID"];
-}
+    if(parseInt(context.opts.cordova.version.split('.')[0], 10) >= 7){
+        var config = fs.readFileSync("package.json").toString();
+        var json = JSON.parse(config);
+        var APP_ID = json["cordova"]["plugins"]["cordova-plugin-facebook4"]["APP_ID"];
+    }
+    else // Cordova < 7, use config.xml
+    {
+        var getPreferenceValue = function(config, name) {
+            var value = config.match(new RegExp('name="' + name + '" value="(.*?)"', "i"));
+            if(value && value[1]) {
+                return value[1]
+            } else {
+                return null
+            }
+        };
 
-var files = [
-    "platforms/browser/www/plugins/cordova-plugin-facebook4/www/facebook-browser.js",
-    "platforms/browser/platform_www/plugins/cordova-plugin-facebook4/www/facebook-browser.js",
-    "platforms/browser/www/cordova.js",
-    "platforms/browser/platform_www/cordova.js"
-]
+        if(process.argv.join("|").indexOf("APP_ID=") > -1) {
+            var APP_ID = process.argv.join("|").match(/APP_ID=(.*?)(\||$)/)[1]
+        } else {
+            var config = fs.readFileSync("config.xml").toString();
+            var APP_ID = getPreferenceValue(config, "APP_ID")
+        }
+    }
 
-for(var i in files) {
-    try {
-    	var contents = fs.readFileSync(files[i]).toString()
-	    fs.writeFileSync(files[i], contents.replace(/APP_ID/g, APP_ID))
-	} catch(err) {}
-}
+    var files = [
+        "platforms/browser/www/plugins/cordova-plugin-facebook4/www/facebook-browser.js",
+        "platforms/browser/platform_www/plugins/cordova-plugin-facebook4/www/facebook-browser.js",
+        "platforms/browser/www/cordova.js",
+        "platforms/browser/platform_www/cordova.js"
+    ];
+
+    for(var i in files) {
+        try {
+            var contents = fs.readFileSync(files[i]).toString();
+            fs.writeFileSync(files[i], contents.replace(/APP_ID/g, APP_ID))
+        } catch(err) {}
+    }
+};
